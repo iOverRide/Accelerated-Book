6073281d1bd7cc5e3036ab7b7557ae08e668ed02
iOverRide <Override@hotmail.co.uk>
2014-07-22 19:30:38 +0100

    ch0

---
+++ ch0/0-3/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!\t tab ends up here \t";
+    std::cout << "And here" << std::endl;
+    std::cout << "\t \t \t here";
+    return 0;
+}
---
+++ ch0/0-4/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "The hello world program." << std::endl;
+    std::cout << "#include <iostream>" << std::endl;
+    std::cout << "\n";
+    std::cout << "int main()\n";
+    std::cout << "{\n";
+    std::cout << "\t std::cout << \"Hello World\"  << std::endl; \n";
+    std::cout << "\n";
+    std::cout << "\t return 0;\n";
+    std::cout << "}";
+
+
+    return 0;
+}
